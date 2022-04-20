2f206ccaf7c0877547f3bfbc13e32dad3736995d
fabiostb <santiago.fabio@gmail.com>
2022-04-20 11:06:08 -0400

    #firt commit

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'pokemon.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  t = Pokemon.new
+  t.name = row['Name']
+  t.type1 = row['Type 1']
+  t.type2 = row['Type 2']
+  t.total = row['Total']
+  t.hp = row['HP']
+  t.attack = row['Attack']
+  t.defense = row['Defense']
+  t.special_attack = row['Sp. Atk']
+  t.special_defense = row['Sp. Def']
+  t.speed = row['Speed']
+  t.generation = row['Generation']
+  t.legendary = 'true' == (row['Legendary'])&.downcase! ? 1 : 0
+  t.save
+end
